72d06c3c8644cbc921020dd3024d90670e2e904c
Im-perfect <zhangke.kerri@gmail.com>
2019-12-09 00:07:26 +0100

    switch account, toggle account list

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Account from "./Account"
 
 export class HeaderBar extends Component {
     render() {
         return (
             <div>
-                
+                <Account />
             </div>
         )
     }
--- src/reducers/currentAccount.js
+++ src/reducers/currentAccount.js
@@ -1,9 +1,14 @@
 import { accounts } from "../data.json";
 
-const initialStateMail = accounts[0].mail.sort((a, b) => b.date-a.date)
+const initialStateMail = accounts[0].mail.sort((a, b) => b.date - a.date);
 
-const reducer = (state = {...accounts[0], mail: initialStateMail}, action = {}) => {
+const reducer = (
+  state = { ...accounts[0], mail: initialStateMail },
+  action = {}
+) => {
   switch (action.type) {
+    case "SWITCH_ACCOUNT":
+      return {...accounts[action.accountIndex]};
     case "SET_EMAIL_READ":
       const newMail = state.mail.map(mail =>
         mail.date === action.date ? { ...mail, read: "true" } : mail
